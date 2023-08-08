0ad618f33c0cdc8f32a5c95cfd0fb1cbfea6c918
aefayoub <aef.ayoub@gmail.com>
2023-08-08 07:57:59 -0700

    cmt task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(10):
     for j in range(i + 1, 10):
          comb = (i * 10) + j
          if comb < 89:
-            print("{:02d}".format(comb), end=", ")
+             print("{:02d}".format(comb), end=", ")
          else:
-            print("89")
+             print("89")
