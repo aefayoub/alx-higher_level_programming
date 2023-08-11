376aef1e23849b67134afafa1c2689262bfecc26
aefayoub <aef.ayoub@gmail.com>
2023-08-11 04:07:33 -0700

    cmt tsak 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     else:
         print(f"{nargs} {'argument' if nargs == 1 else 'arguments'}:")
 
-        for i, arg in enumerate(argv, start = 1):
+        for i, arg in enumerate(argv, start=1):
             print(f"{i}: {arg}")
