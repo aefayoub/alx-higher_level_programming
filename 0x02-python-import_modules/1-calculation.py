12b6d2c3217253fa56ee29ffa91042a0fa162f69
aefayoub <aef.ayoub@gmail.com>
2023-08-11 03:50:04 -0700

    cmt task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 
-add = __import__('calculator_1').add
-sub = __import__('calculator_1').sub
-mul = __import__('calculator_1').mul
-div = __import__('calculator_1').div
-
-a = 10
-b = 5
-
-print(f"{a} + {b} = {a + b}")
-print(f"{a} - {b} = {a - b}")
-print(f"{a} * {b} = {a * b}")
-print(f"{a} / {b} = {int(a / b)}")
+if __name__ = "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print(f"{a} + {b} = {a + b}")
+    print(f"{a} - {b} = {a - b}")
+    print(f"{a} * {b} = {a * b}")
+    print(f"{a} / {b} = {int(a / b)}")
