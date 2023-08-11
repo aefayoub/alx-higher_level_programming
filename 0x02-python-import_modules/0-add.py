fd1c5638949442c1bfaf7081cc24160907de5ee0
aefayoub <aef.ayoub@gmail.com>
2023-08-11 02:35:12 -0700

    cmt task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-add = __import__('add_0').add
+from add_0 import add
 
 a = 1
 b = 2
-print ("{} + {}  = {}".format(a, b, add(a, b)))
+print("{} + {}  = {}".format(a, b, add(a, b)))
