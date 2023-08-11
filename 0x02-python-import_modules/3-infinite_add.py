00f42def8875b5b67bb987de26c1fdc65fa5f041
aefayoub <aef.ayoub@gmail.com>
2023-08-11 04:19:38 -0700

    cmt task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
-import sys
-
-def arguments():
+if __name__ == "__main__":
+    import sys
     argv = sys.argv[1:]
     nargs = len(argv)
     sumargs = 0
     for i, arg in enumerate(argv, start = 1):
         sumargs += int(arg)
-    print (sumargs)
-
-arguments()
+    print(sumargs)
