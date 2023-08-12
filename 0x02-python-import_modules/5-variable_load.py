983494a39ffc469b9c50a3ff03f45054acf2e63e
aefayoub <aef.ayoub@gmail.com>
2023-08-12 05:07:24 -0700

    cmt task 5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
-import importlib.util
+if __name__ == "__main__":
 
-spec = importlib.util.spec_from_file_location("variable_load_5", "variable_load_5.py")
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
-
-print(f"{module.a}")
+    from variable_load_5 import a
+    print(a)
