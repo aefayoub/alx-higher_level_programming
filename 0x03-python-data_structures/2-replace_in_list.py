cf6429473711af434da61fd8d2f3fa80f19aed90
aefayoub <aef.ayoub@gmail.com>
2023-08-14 14:24:06 -0700

    cmt task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,8 +3,8 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    if idx > len(my_list):
+    elif idx > len(my_list):
         return my_list
-
-    my_list[idx] = element
+    else:
+        my_list[idx] = element
     return my_list
