fa0fbb963462777bbedde41d90c63e29833c2ae7
aefayoub <aef.ayoub@gmail.com>
2023-08-15 01:25:39 -0700

    cmt task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,9 +3,5 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     new_list[idx] = element
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list) - 1:
-        return my_list
-    else:
+    if 0 <= idx < len(new_list):
         return new_list
