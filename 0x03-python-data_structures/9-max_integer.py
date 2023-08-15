a049466a6012b0b0f6a682f8f959d7dc7e2b2a2f
aefayoub <aef.ayoub@gmail.com>
2023-08-15 08:06:48 -0700

    cmt rask 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
     max_value = 0
-    for i in range(len(my_list)):
+    for i in range(len(my_list) - 1):
         if my_list[i] > max_value:
             max_value = my_list[i]
     return (max_value)
