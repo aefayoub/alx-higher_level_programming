ac492304c9ef6d9391e31bf8950182fe3066997e
aefayoub <aef.ayoub@gmail.com>
2023-08-28 06:50:51 -0700

    cmt task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 def safe_print_division(a, b):
     try:
         rslt = (a / b)
-    except:
+    except Exception:
         rslt = None
     finally:
         print("Inside result: {}".format(rslt))
