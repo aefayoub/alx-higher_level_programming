284f224a6a95bfbd9c4239783856a919dd445785
aefayoub <aef.ayoub@gmail.com>
2023-08-28 05:52:05 -0700

    cmt task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,10 +5,9 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-        except Exception:
-            break
-        else:
+        except:
             nbr += 1
+            break
 
     print()
     return(nbr)           
