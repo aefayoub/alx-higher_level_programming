9c7ac695df23b8033cb8fe1d43aa0b3aabae34e9
aefayoub <aef.ayoub@gmail.com>
2023-08-29 08:18:28 -0700

    cmt task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Define a class square"""
 class Square:
+
+    """Class Square that defines a square object"""
     def __init__(self, size):
         """Initialize method that stores the size of the square
 
