bb2ca145abfcb73b9612723a1bb7de5b6e138624
aefayoub <aef.ayoub@gmail.com>
2023-08-30 07:19:26 -0700

    cm task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+"""
+This is a module-level docstring. It provides an overview of the class square.
+"""
+
+
+class Square:
+    """
+    The Square class represents a square shape.
+
+    Attributes:
+        __size (int): The size of the square's sides.
+
+    Methods:
+        __init__(self, size=0): Initializes a new Square instance.
+
+    Raises:
+        TypeError: If the provided size is not an integer.
+        ValueError: If the provided size is a negative integer.
+
+    Note:
+        Double underscores before a variable or method name, like __size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int, optional): The size of the square's sides.
+
+        Raises:
+            TypeError: If the provided size is not an integer.
+            ValueError: If the provided size is a negative integer.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return (self.__size ** 2)
+    
+    def my_print(self):
+        """ 
+        Method that prints a # square according
+        to the size value
+        """
+        if not self.__size:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
