d67eda4cd9d9991be76485bd6fcc95084f54c6c0
aefayoub <aef.ayoub@gmail.com>
2023-09-05 10:16:00 -0700

    cmt task 1 2 3

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""
+This module is composed by a function that divides the numbers of a matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    """ Function that divides the integer/float numbers of a matrix
+
+    Args:
+        matrix: list of a lists of integers/floats
+        div: number which divides the matrix
+
+    Returns:
+        A new matrix with the result of the division
+
+    Raises:
+        TypeError: If the elements of the matrix aren't lists
+                   If the elemetns of the lists aren't integers/floats
+                   If div is not an integer/float number
+                   If the lists of the matrix don't have the same size
+
+        ZeroDivisionError: If div is zero
+
+
+    """
+    
+    if not type(div) in (int, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    msg_type = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if not matrix or not isinstance(matrix, list):
+        raise TypeError(msg_type)
+
+    len_e = 0
+    msg_size = "Each row of the matrix must have the same size"
+
+    for i in matrix:
+        if not i or not isinstance(i, list):
+            raise TypeError(msg_type)
+
+        if len_e != 0 and len(i) != len_e:
+            raise TypeError(msg_size)
+
+        for nbr in i:
+            if not type(nbr) in (int, float):
+                raise TypeError(msg_type)
+        len_e = len(i)
+
+    m = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
+    return (m)
