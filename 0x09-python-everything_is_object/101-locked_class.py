ffa6f7070fc68e92c06288df65bdca3ed0e8ff30
aefayoub <aef.ayoub@gmail.com>
2023-09-06 05:55:43 -0700

    cmt task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+
 
 """
 This is a module that containts a clas that avoids
@@ -7,6 +7,17 @@ dynmaically created attributes
 
 
 class LockedClass:
+    """
+    A class with restricted attribute.
+
+    Attributes:
+        first_name (str): The first name.
+
+    Note:
+        This class uses __slots__ to limit attribute.
+        Attempts to create or access other attributes.
+    """
+
     __slots__ = ["first_name"]
 
     def __init__(self):
