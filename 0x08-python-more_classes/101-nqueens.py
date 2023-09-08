fd8d779a0bcfab0b64ec2c589137bc4dc3b496fc
aefayoub <aef.ayoub@gmail.com>
2023-09-08 04:42:32 -0700

    cmt task 101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -62,7 +62,7 @@ def Queen(m_queen, nqueen):
 
     m_queen[nqueen] = -1
 
-    while((m_queen[nqueen] < len(m_queen) - 1)):
+    while ((m_queen[nqueen] < len(m_queen) - 1)):
 
         m_queen[nqueen] += 1
 
