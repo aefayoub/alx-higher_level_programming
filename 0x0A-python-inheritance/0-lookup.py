659484d4268a65ed3f50c475fd0d37e16f957fdc
aefayoub <aef.ayoub@gmail.com>
2023-09-11 12:59:49 -0700

    cmt task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+MOdule lookup
+"""
+
+
+def lookup(obj):
+    """ Function that returns the list of available attributes
+        and methods of an object
+
+    Args:
+        obj: instance of the class
+
+    Returns:
+        List of attributes
+    """
+
+    return dir(obj)
