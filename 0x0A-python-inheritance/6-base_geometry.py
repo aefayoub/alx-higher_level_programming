e108a9a936858310de230bb36eb755c073de7a16
aefayoub <aef.ayoub@gmail.com>
2023-09-11 15:37:58 -0700

    cmt task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Module based on Module 5
+"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
