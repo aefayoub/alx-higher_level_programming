f92a206114ae0cae989a7cd709c6931e70757786
aefayoub <aef.ayoub@gmail.com>
2023-09-12 08:13:57 -0700

    cmt task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Import BaseGeometry class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+"""
+Module based on Module 5
+"""
+
+
+class Square(Rectangle):
+    """Class contains __int__ function"""
+
+    def __init__(self, size):
+        """Initialisation"""
+
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Method that returns the area of the instance"""
+        return super().area()
