d96e3d612c5331bffb76091b6f59bb66079860e3
aefayoub <aef.ayoub@gmail.com>
2023-09-12 07:45:05 -0700

    cmt task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""
+Module based on Module 5
+"""
+
+
+class Rectangle(BaseGeometry):
+    """Class contains __int__ function"""
+
+    def __init__(self, width, height):
+        """Initialisation"""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("heigt", height)
