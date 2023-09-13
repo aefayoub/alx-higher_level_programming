103c8359410779e45d541bd3f45435a9d8ca3e68
aefayoub <aef.ayoub@gmail.com>
2023-09-13 06:22:57 -0700

    cmt task *

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,6 +5,7 @@ Module that returns the dictionary description with a simple
 data structure for a JSON serialization of an object
 """
 
+
 def class_to_json(obj):
     """Functuon returns the description of an obj."""
 
