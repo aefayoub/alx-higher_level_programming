9abc56f5245496f7912ff96edd4cf5546a0b5a22
aefayoub <aef.ayoub@gmail.com>
2023-09-14 07:26:19 -0700

    cmt task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-"""Module append after"""
+"""
+Module append after
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
@@ -14,10 +16,13 @@ def append_after(filename="", search_string="", new_string=""):
 
     res_line = []
     with open(filename, 'r', encoding="utf-8") as f:
+        """append content file"""
+
         for line in f:
             res_line += [line]
             if line.find(search_string) != -1:
             res_line += [new_string]
 
     with open(filename, 'w', encoding="utf-8") as f:
+        """write content file"""
         f.write("".join(res_line))
