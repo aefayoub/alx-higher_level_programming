dfa2319c316963c425f8f7a6f189c8e82804a725
aefayoub <aef.ayoub@gmail.com>
2023-09-17 12:33:46 -0700

    cmt class rectangle and base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Module that contains class Base
+"""
+
+
+class Base:
+    """class Base"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """ Initializes instances"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
