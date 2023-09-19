f9e1874cad662774f9ec23ce46d9acd0e656fea4
aefayoub <aef.ayoub@gmail.com>
2023-09-19 04:12:09 -0700

    cmt class base - coorect errors

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,8 +19,9 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """List to json string"""
-        if list_dictionaries is None or list_dictionaries == []:
-            return []
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
         return json.dumps(list_dictionaries)
