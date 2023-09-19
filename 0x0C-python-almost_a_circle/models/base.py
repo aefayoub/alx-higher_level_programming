ccb34e1df3179a804eef288673f4fac9aceb72e1
aefayoub <aef.ayoub@gmail.com>
2023-09-19 03:57:30 -0700

    cmt class base add method to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""imports"""
+import json
 
 """
 Module that contains class Base
@@ -16,3 +18,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """List to json string"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return []
+        return json.dumps(list_dictionaries)
