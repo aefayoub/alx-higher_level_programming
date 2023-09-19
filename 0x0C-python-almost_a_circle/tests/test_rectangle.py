27b27cd0d706fa6a21867aa196aa30969063f5e7
aefayoub <aef.ayoub@gmail.com>
2023-09-19 08:42:06 -0700

    cmt test

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,31 @@
+import unittest
+from models.rectangle import Rectangle  # Adjust the import path as needed
+
+class TestRectangle(unittest.TestCase):
+    def setUp(self):
+        # Add setup code if needed
+        pass
+
+    def test_area(self):
+        # Add test cases for the 'area' method
+        pass
+
+    def test_display(self):
+        # Add test cases for the 'display' method
+        pass
+
+    def test_str(self):
+        # Add test cases for the '__str__' method
+        pass
+
+    def test_update(self):
+        # Add test cases for the 'update' method
+        pass
+
+    def test_to_dictionary(self):
+        # Add test cases for the 'to_dictionary' method
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
