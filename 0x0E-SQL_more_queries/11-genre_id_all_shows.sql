ffd03c27601b3942f5dd2c555dd1f92cc0ecde7d
aefayoub <aef.ayoub@gmail.com>
2023-10-20 07:16:12 -0700

    cmt tas 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- script that lists all shows contained in hbtn_0d_tvshows.
+SELECT ts.title AS title, tvsg.genre_id AS genre_id
+FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tvsg ON tvsg.show_id = ts.id
+ORDER BY title, genre_id ASC;
