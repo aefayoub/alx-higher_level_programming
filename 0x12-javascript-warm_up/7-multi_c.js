9a6f794e1157bb9733859aea73524478fb4f9ef1
aefayoub <aef.ayoub@gmail.com>
2023-11-13 08:09:02 -0800

    cmt task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myarray = process.argv
+for (let i = 0; i < myarray.length; i++) {
+  let number = parseInt(myarray[2]);
+  if (Number.isNaN(number)) {
+    console.log('Missing number of occurrences');
+    break;
+  } else {
+    console.log(myarray[i]);
+  }
+}
