c759cfa37b7c107c1ebdf35c3c6b685a3a09d9d8
aefayoub <aef.ayoub@gmail.com>
2023-11-15 05:14:10 -0800

    cmt task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0, x; i < this.height; i++) {
+      x = '';
+      for (let j = 0; j < this.width; j++) {
+        x += 'X';
+      }
+      console.log(x);
+    }
+  }
+  
+  rotate () {
+    const permu = this.height;
+    this.height = this.width;
+    this.width = permu;
+  }
+ 
+  double () {
+   this.width *= 2;
+   this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
