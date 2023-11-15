15af884c9b47357ab015fa1c9578c0c031a5fccc
aefayoub <aef.ayoub@gmail.com>
2023-11-15 05:33:50 -0800

    cmt task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0, x; i < this.height; i++) {
+      x = '';
+      for (let j = 0; j < this.width; j++) {
+        x += c;
+      }
+      console.log(x);
+    }
+  }
+}
+
+module.exports = Square;
