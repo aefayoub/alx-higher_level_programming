defc897c2685a9681840cbd9dc2dd082205bb807
aefayoub <aef.ayoub@gmail.com>
2023-11-18 13:40:15 -0800

    cmt task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+script that lists all states from the database
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states on cities.state_id = states.id \
+    AND states.name = %s;", (sys.argv[4], ))
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
