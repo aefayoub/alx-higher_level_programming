3479e847f50a96377038015dd74972f8a39d3e6b
aefayoub <aef.ayoub@gmail.com>
2023-11-18 13:36:05 -0800

    cmt task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that lists all states from the database
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states on cities.state_id = states.id")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
