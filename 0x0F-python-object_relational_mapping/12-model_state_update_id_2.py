8bc8380c890fb33a4881005624d782ca81858c7d
aefayoub <aef.ayoub@gmail.com>
2023-11-24 13:21:02 -0800

    cmt task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-      
+
     updateState = session.query(State).filter(State.id == 2).first()
 
     if updateState:
