91efed32f78e514a9e244d1f8dc3b92001ed5946
aefayoub <aef.ayoub@gmail.com>
2023-11-24 13:29:15 -0800

    cmt task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 
     selectedStates = session.query(State).filter(State.name.like('%a%')).all()
 
-    for state in selectedStates
+    for state in selectedStates:
         session.delete(state)
 
     session.commit()
