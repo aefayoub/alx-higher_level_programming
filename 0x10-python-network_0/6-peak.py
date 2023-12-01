f7ac1b1a5a2ac478d022c28e58a2f3c96173c4f7
aefayoub <aef.ayoub@gmail.com>
2023-12-01 02:50:13 -0800

    cmt task 6- pycode style

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,9 +15,11 @@ def find_peak(list_of_integers):
     if middle - 1 < 0 and middle + 1 >= length:
         return mylist[middle]
     elif middle - 1 < 0:
-        return mylist[middle] if mylist[middle] > mylist[middle + 1] else mylist[middle + 1]
+        return mylist[middle] if mylist[middle] >
+        mylist[middle + 1] else mylist[middle + 1]
     elif middle + 1 >= length:
-        return mylist[middle] if mylist[middle] > mylist[middle - 1] else mylist[middle - 1]
+        return mylist[middle] if mylist[middle] >
+        mylist[middle - 1] else mylist[middle - 1]
 
     if mylist[middle - 1] < mylist[middle] > mylist[middle + 1]:
         return mylist[middle]
