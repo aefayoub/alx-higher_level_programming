5dd9ba8c2a583ac5d5ec7d2546d03c0b4b9e3c65
aefayoub <aef.ayoub@gmail.com>
2023-12-01 12:25:35 -0800

    cmt 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,8 +5,5 @@ import sys
 
 
 if __name__ == "__main__":
-    try:
-        req = requests.get(sys.argv[1])
-        print(req.headers['X-Request-Id'])
-    except ex:
-        pass
+    req = requests.get(sys.argv[1])
+    print(req.headers['X-Request-Id'])
