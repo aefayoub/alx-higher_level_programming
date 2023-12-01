f562436bee846fbbf86386a39d64f063831c8027
aefayoub <aef.ayoub@gmail.com>
2023-12-01 11:55:37 -0800

    cmt task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+
+if __name__ == "__main__":
+    try:
+        req = requests.get(sys.argv[1])
+        print(req.headers['X-Request-Id'])
+    except:
+        pass
