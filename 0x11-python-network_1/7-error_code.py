b46e11aacd21066a874383826f281ed3b6aae210
aefayoub <aef.ayoub@gmail.com>
2023-12-01 12:15:31 -0800

    cmt 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    req = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
