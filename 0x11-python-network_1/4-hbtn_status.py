ad08b1c269693ca8025d6a21dafa522386547203
aefayoub <aef.ayoub@gmail.com>
2023-12-01 07:04:03 -0800

    cmt task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 
 if __name__ == "__main__":
     req = requests.get("https://alx-intranet.hbtn.io/status")
-    txtdata = req.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(txtdata), txtdata))
+    d = req.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(d), d))
