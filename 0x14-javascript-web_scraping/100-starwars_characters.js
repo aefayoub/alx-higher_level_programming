0ada389db7f823975b135f4bee6b31ec9e8de8bf
= <=>
2024-01-03 14:17:26 -0800

    cmt task 100

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const req = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+req(url, function (error, response, body) {
+  if (error == null) {
+    const reslt = JSON.parse(body);
+    const charac = reslt.characters;
+    for (let i = 0; i < charac.length; i++) {
+      req(charac[i], function (error, response, body){
+        if (error == null) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
